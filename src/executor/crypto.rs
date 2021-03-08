6f9015c520e5594ef01d495e71d11c9e4b2436b8
Mikhail Komarov <nemo@nil.foundation>
2021-03-08 17:38:13 +0300

    Minor symbol import fixes #1

--- src/executor/crypto.rs
+++ src/executor/crypto.rs
@@ -26,7 +26,6 @@ use crate::{
     },
     types::{Exception, Failure}
 };
-use crusty3_zk::create_random_proof;
 use sha2::Digest;
 use ed25519::signature::{Signature, Verifier};
 use std::sync::Arc;
